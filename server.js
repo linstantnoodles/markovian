11c185b7dd76d8c0dcfdea3259bf00865de12df2
Alan Lin <linpd2@gmail.com>
2013-04-18 16:14:22 -0400

    updated formatting to spaces

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-var express = require('express');
-var handlers = require('./handlers');
-var url   = require('url')
-var app = express();
+var express = require('express'),
+    handlers = require('./handlers'),
+    url = require('url'),
+    app = express();
 
 app.use(express.static(__dirname + '/assets'));
 
 app.get('/', function(req, res) {
-	 res.sendfile(__dirname + '/assets/' + 'index.html');
+  res.sendfile(__dirname + '/assets/' + 'index.html');
 });
 
 app.get('/text/:id', function(req, res) {
-	handlers.getText(res, req.param('id'));
+  handlers.getText(res, req.param('id'));
 });
 
 app.listen(8888);
